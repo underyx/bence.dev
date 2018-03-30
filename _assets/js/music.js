1f0738d8dce85e32c443ab8747eb2a0aeccd2ce9
Bence Nagy <bence@underyx.me>
2018-03-30 22:32:00 +0200

    Rework /music page

--- _assets/js/music.js
+++ _assets/js/music.js
@@ -1,21 +1,7 @@
-$(document).ready(function() {
-  $(".music .content table button").bind("click", function() {
-    if (!$(this).hasClass("active")) {
-      $(".music .content .placeholder").remove();
-      $(".grooveshark > param").attr("value", "playlistID=" + $(this).data("grooveshark") + "&p=1");
-      $(".grooveshark").css("display", "block");
-
-      var obj = $(".grooveshark");
-      var orig = obj.html();
-      obj.html(orig);
-
-      $("button").removeClass("active");
-      $(this).addClass("active");
-
-      _gaq.push(['_trackEvent', 'Playlists', 'Play (Grooveshark)', $(this).data("grooveshark").toString()]);
-      setTimeout(function(){$(".grooveshark").css("opacity", "1");}, 200);
-    } else {
-    _gaq.push(['_trackEvent', 'Playlists', 'Play (Disabled)', $(this).data("grooveshark").toString()]);
-    }
-  });
+$(function() {
+  $('.music .content #launch').click(function() {
+    $('body').addClass('launched');
+    setTimeout(function () { $('.site').hide(); }, 1000);
+    setTimeout(function () { document.querySelector('#swipe animate').beginElement(); }, 1600);
+  })
 });
