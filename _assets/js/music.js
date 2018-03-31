18992176c698eccbae0eff0b0082d79eb43ac850
Bence Nagy <bence@underyx.me>
2018-03-31 14:14:42 +0200

    music: Add generic playlist switching code

--- _assets/js/music.js
+++ _assets/js/music.js
@@ -1,11 +1,17 @@
+switchToPlaylist = function (playlistId) {
+  const playlist = playlists[playlistId];
+  document.getElementById('player-embed').src = `https://open.spotify.com/embed/user/underyx/playlist/${playlist.spotify_id}`;
+  document.getElementById('background-over').src = `${playlist.image_url}`;
+  setTimeout(function () {
+    document.querySelector('#swipe rect').classList.replace('left', 'middle');
+  }, 2000);
+}
+
 document.querySelector('.music .content #launch').addEventListener('click', function() {
   document.getElementsByTagName('body')[0].classList.add('launched');
   setTimeout(function () {
     document.getElementsByClassName('site')[0].style.display = 'none';
     document.getElementsByClassName('player')[0].style.display = 'block';
   }, 1000);
-  setTimeout(function () {
-    document.querySelector('#swipe animate').beginElement();
-    document.getElementById('player-embed').src = 'https://open.spotify.com/embed/user/underyx/playlist/4AL4GXMflWxZ42UJLDQYJf';
-  }, 1600);
+  switchToPlaylist('2018-03');
 });
