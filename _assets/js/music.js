3a5ab45def87dcf812c7b94d934808b09002d73a
Bence Nagy <bence@underyx.me>
2018-03-31 11:28:37 +0200

    music: Fix scrollbar popping up when launching

--- _assets/js/music.js
+++ _assets/js/music.js
@@ -1,6 +1,9 @@
 document.querySelector('.music .content #launch').addEventListener('click', function() {
   document.getElementsByTagName('body')[0].classList.add('launched');
-  setTimeout(function () { document.getElementsByClassName('site')[0].style.display = 'none'; }, 1000);
+  setTimeout(function () {
+    document.getElementsByClassName('site')[0].style.display = 'none';
+    document.getElementsByClassName('player')[0].style.display = 'block';
+  }, 1000);
   setTimeout(function () {
     document.querySelector('#swipe animate').beginElement();
     document.getElementById('player-embed').src = 'https://open.spotify.com/embed/user/underyx/playlist/4AL4GXMflWxZ42UJLDQYJf';
