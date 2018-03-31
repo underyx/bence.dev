7b3f87225ad6284c075a10009a44cd5de364cbcc
Bence Nagy <bence@underyx.me>
2018-03-31 10:27:06 +0200

    music: Fix bugs on Firefox and Chrome Mobile

--- _assets/js/music.js
+++ _assets/js/music.js
@@ -1,7 +1,8 @@
-$(function() {
-  $('.music .content #launch').click(function() {
-    $('body').addClass('launched');
-    setTimeout(function () { $('.site').hide(); }, 1000);
-    setTimeout(function () { document.querySelector('#swipe animate').beginElement(); }, 1600);
-  })
+document.querySelector('.music .content #launch').addEventListener('click', function() {
+  document.getElementsByTagName('body')[0].classList.add('launched');
+  setTimeout(function () { document.getElementsByClassName('site')[0].style.display = 'none'; }, 1000);
+  setTimeout(function () {
+    document.querySelector('#swipe animate').beginElement();
+    document.getElementById('player-embed').src = 'https://open.spotify.com/embed/user/underyx/playlist/4AL4GXMflWxZ42UJLDQYJf';
+  }, 1600);
 });
