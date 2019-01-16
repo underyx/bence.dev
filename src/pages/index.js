fd1dd0d8157214ef49284c700e41e464cd1e8a24
Bence Nagy <bence@underyx.me>
2019-01-16 21:28:47 +0100

    Create base of new, gatsby-based site

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>There's no content yet, but it's a start!</Layout>
+)
+
+export default IndexPage
