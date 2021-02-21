7930b075d7d2c9ec1db9b5b084dac2c373217c6a
Bence Nagy <bence@underyx.me>
2021-02-21 18:14:28 +0100

    Switch to preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    `gatsby-plugin-preact`,
     `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-fullstory`,
