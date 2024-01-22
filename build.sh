3119b629667950c37f2f78b74e1c9664e1cf8f1d
Bence Nagy <nagy@bence.dev>
2024-01-22 10:18:06 -0800

    pick correct score

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export OURA_SLEEP_SCORE=$(curl https://api.ouraring.com/v2/usercollection/daily_sleep -H "Authorization: Bearer $OURA_PAT" | jq .data[0].score)
+export OURA_SLEEP_SCORE=$(curl https://api.ouraring.com/v2/usercollection/daily_sleep -H "Authorization: Bearer $OURA_PAT" | jq .data[-1].score)
 
 pnpm build
